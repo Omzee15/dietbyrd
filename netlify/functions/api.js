e48ff179706503f7b844629e01a0d55709ce071e
Arunodoy18 <arunodoy630@gmail.com>
2026-05-31 11:44:16 +0530

    Load Netlify api app via dynamic import

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,12 @@
 const serverless = require("serverless-http");
-const appModule = require("../../api/_app.js");
 
-const app = appModule?.default || appModule;
+let cachedHandler;
 
-exports.handler = serverless(app, {
-  basePath: "/.netlify/functions/api",
-});
+exports.handler = async (event, context) => {
+  if (!cachedHandler) {
+    const { default: app } = await import("../../api/_app.js");
+    cachedHandler = serverless(app, { basePath: "/.netlify/functions/api" });
+  }
+
+  return cachedHandler(event, context);
+};
